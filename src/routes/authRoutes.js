35b0e0f7a94e711fa7a330281068b4a80c8521ff
Ricardo Tafur <tafurcaraballora@gmail.com>
2026-05-03 00:13:17 -0500

    Backend de la pantalla de registro

---
+++ src/routes/authRoutes.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import { register } from '../controllers/authController.js';
+
+const router = express.Router();
+
+// Ruta para registrar un nuevo usuario
+router.post('/register', register);
+
+export default router;
